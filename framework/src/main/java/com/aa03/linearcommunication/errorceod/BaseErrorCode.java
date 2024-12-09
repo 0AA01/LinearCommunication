69688b67408cc861ec94ddb99ad2fed37d228465
AA <1435220156@qq.com>
2024-12-09 16:21:07 +0800

    feat: 错误码定义

---
+++ framework/src/main/java/com/aa03/linearcommunication/errorceod/BaseErrorCode.java
@@ -0,0 +1,33 @@
+package com.aa03.linearcommunication.errorceod;
+
+/**
+ * 基础错误码定义
+ * author: xzj
+ * 开发时间: 2024-12-09
+ */
+public enum BaseErrorCode implements IErrorCode {
+
+    // ========== 一级宏观错误码 客户端错误 ==========
+    CLIENT_ERROR("A000001", "用户端错误"),
+
+    // ========== 二级宏观错误码 系统请求缺少幂等Token ==========
+    IDEMPOTENT_TOKEN_NULL_ERROR("A000200", "幂等Token为空");
+
+    private final String code;
+    private final String message;
+
+    BaseErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
---
+++ framework/src/main/java/com/aa03/linearcommunication/errorceod/IErrorCode.java
@@ -0,0 +1,19 @@
+package com.aa03.linearcommunication.errorceod;
+
+/**
+ * 错误码抽象接口
+ * author: xzj
+ * 开发时间: 2024-12-09
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
